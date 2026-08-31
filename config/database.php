3aefc3a3ec7215698d5dc843bc1796a72184fcf0
Alisa Jauharo F <alisajauharofirdausa@gmail.com>
2026-08-31 20:13:49 +0700

    upload config

---
+++ config/database.php
@@ -0,0 +1,25 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Content-Type: application/json; charset=UTF-8");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+
+$host = "localhost";
+$user = "root";
+$password = "";
+$dbname = "db_inventory";
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+} catch (PDOException $e) {
+    echo json_encode(["error" => "Koneksi Database Gagal: " . $e->getMessage()]);
+    exit();
+}
+?>
